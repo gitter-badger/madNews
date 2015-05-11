8f73240036141f5d55004b539cb03b87b0c795f5
Andrey Bereza <abereza90@gmail.com>
2015-05-11 18:30:02 +0300

    REST configuring

--- src/main/java/org/madnews/controller/NewsController.java
+++ src/main/java/org/madnews/controller/NewsController.java
@@ -1,23 +1,29 @@
 package org.madnews.controller;
 
+import org.madnews.entity.Post;
 import org.madnews.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value="/api/v1/public")
 public class NewsController {
 
     @Autowired
     private PostService postService;
 
-    @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public @ResponseBody List getPosts(){
+    @RequestMapping(value ="/news/{id}")
+    public Post getPost(@PathVariable Long id){
+        return postService.readPost(id);
+    }
+
+    @RequestMapping(value = "/news", method = RequestMethod.GET)
+    public List getNews(){
         return postService.getPosts();
     }
 }
